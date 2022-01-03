b9a6b5fd0cc09d977aa7cf801791525c8fbc41b4
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-03 19:44:12 +0200

    KeyError tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,5 +22,17 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
        
+    @parameterized.expand([
+    ({}, ("a",)),
+    ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ 
+        Test that access nested map raises a KeyError
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual((path[len(path) - 1]), context.exception.args[0])
+        
 if __name__ == '__main__':
     unittest.main()
