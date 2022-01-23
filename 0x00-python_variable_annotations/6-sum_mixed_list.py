7c264ddaf3094ee236089a95d8da0e3a0821dbe6
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-23 14:56:47 +0200

    Fixing test error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,5 +3,5 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     return sum(mxd_lst)
