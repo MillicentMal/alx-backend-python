bddb15b27de8d81b48edff00d7d58c5477e6ffce
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-23 14:40:52 +0200

    Fixing syntax error

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,7 +2,7 @@
 """
      Defining variables
     """
-a : int = 1
+a: int = 1
 pi: float = 3.14 # with a value of 3.14
 i_understand_annotations: bool = True # a boolean with a value of True
 school: str = 'Holberton'
