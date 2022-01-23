8e3341ca3700c512b21ce33b899ee9d0160d3cca
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-23 15:26:48 +0200

    Duck typing

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,5 +5,3 @@ from typing import Iterable, List, Sequence, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]
-
-print(element_length.__annotations__)
