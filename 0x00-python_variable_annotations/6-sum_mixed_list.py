ed4e2a5e189112e9f63e3016087753e7d8d7190f
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-23 14:52:48 +0200

    Task 6: Mixed types in list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List(float, int)) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     return sum(mxd_lst)
