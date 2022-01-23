4d9bae4424a6713e30713e789e1bf94c7dc9bb7f
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-23 14:31:28 +0200

    Floor function

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-from collections.abc import Callable
+
 import math
 
 
-def floor(n: float) -> Callable[[math.floor()]]:
-    math.floor(n)
+def floor(n : float) -> int:
+    return math.floor(n)
