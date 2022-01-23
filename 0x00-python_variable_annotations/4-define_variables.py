a30ca45760cce82f3bc41a67ff3706ba79e9c904
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-23 14:39:39 +0200

    Attempting variables task

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+     Defining variables
+    """
+a : int = 1
+pi: float = 3.14 # with a value of 3.14
+i_understand_annotations: bool = True # a boolean with a value of True
+school: str = 'Holberton'
