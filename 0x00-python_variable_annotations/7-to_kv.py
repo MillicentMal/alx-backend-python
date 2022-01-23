0925d09d79def8eabb4e8d63ff048c0d3164126b
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-23 15:11:44 +0200

    Fixing type error

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from typing import Union
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     return k, v**2
