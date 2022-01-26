c1d5c14fab20e72a8f2aa8c5d0d0a3ab046cee8e
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-26 14:52:26 +0200

    Task 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -17,10 +17,5 @@ async def wait_random(max_delay: int = 10) -> float:
     
     wait_value = random.uniform(0, max_delay)
     await asyncio.sleep(wait_value)
-    
-    # return wait_value
+    return wait_value
 
-
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+    await_random function
+    """
+import  asyncio
+from time import time
+
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n : int, max_delay: int = 0) -> List[float]:
+    """
+    
+
+    Args:
+        n (int): Number of times wait_Random is run
+        max_delay (int, optional): Maximum delay. Defaults to 0.
+
+    Returns:
+        List[float]: Of delays
+    """
+    wait_list: List[float] = []
+    time_delays: List[float] = []
+    for i in range(0, n):
+        wait_list.append(wait_random(max_delay))
+    
+    for wait in asyncio.as_completed(wait_list):
+        task = await wait
+        time_delays.append(task)
+
+    return time_delays
+
