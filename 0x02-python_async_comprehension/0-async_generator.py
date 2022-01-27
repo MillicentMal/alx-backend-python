4d3a01959a0e0ec6ac54b906bedf8b45c73639c9
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-27 23:07:22 +0200

    Adding await

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,5 +13,5 @@ async def async_generator():
         list: of random numbers
     """
     for i in range(10):
-        asyncio.sleep(1)
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
