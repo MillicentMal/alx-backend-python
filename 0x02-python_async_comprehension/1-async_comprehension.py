cf12c1aabfb0a6619b86ca5a28781048301c8c99
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-27 23:32:31 +0200

    Annotation error

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,12 @@
     Makes use of the generator in async_generator
     """
 import asyncio
-from typing import Generator
+from pickletools import float8
+from typing import Generator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def  async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """Import async_generator from the previous task 
     and then write a coroutine called async_comprehension
     that takes no arguments.
