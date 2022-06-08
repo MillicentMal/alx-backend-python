c6e1fbe156802b0dca5fb7be15339aef5aca5bdc
Millicent Malinga <90625689+MillicentMalinga@users.noreply.github.com>
2022-06-08 18:00:00 +0200

    mypy code

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Fixing typng using mypy
+"""
+from typing import Any, List, Tuple
+
+
+def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List:
+    """
+    mypy used to fix typing errors
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
