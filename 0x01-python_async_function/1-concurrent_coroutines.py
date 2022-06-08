c07ef2fc86c4c57b96ac94d2e4eecba3d254d685
Millicent Malinga <90625689+MillicentMalinga@users.noreply.github.com>
2022-06-08 13:11:03 +0200

    fixing pep8 errors

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int=10) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
 
 
