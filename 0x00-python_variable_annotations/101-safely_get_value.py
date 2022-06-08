12870000e6b7f54f6c04a0ff044ff50954b34edf
Millicent Malinga <90625689+MillicentMalinga@users.noreply.github.com>
2022-06-08 17:49:59 +0200

    pycodestyle errors

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,8 +8,8 @@ from typing import Any, Mapping, TypeVar, Union
 
 def safely_get_value(dct: Mapping, key: Any, default:
                      Union[TypeVar('T'), None]
-                     = None) -> Union[Any, 
-                     TypeVar('T')]:
+                     = None) -> Union[Any,
+                                      TypeVar('T')]:
     """
     Using TypeVar
     """
