b08f4b8922da052470d29a4042217879036d0b78
Millicent Malinga <90625689+MillicentMalinga@users.noreply.github.com>
2022-06-08 17:39:27 +0200

    indentation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,7 +7,7 @@ from typing import Any, Mapping, TypeVar, Union
 
 
 def safely_get_value(dct: Mapping, key: Any, default:
-            Union[TypeVar('T'), None] = None) -> Union[Any, TypeVar('T')]:
+                    Union[TypeVar('T'), None] = None) -> Union[Any, TypeVar('T')]:
     """
     Using TypeVar
     """
